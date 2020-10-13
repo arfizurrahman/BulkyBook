d5026a8a03228a6670753396dfc9457aff67a4b2
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-13 15:05:06 +0600

    Added datatable to index page

---
+++ BulkyBook/BulkyBook/wwwroot/js/category.js
@@ -0,0 +1,26 @@
+﻿var dataTable;
+
+$(function() {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        'ajax': {
+            'url': '/Admin/Category/GetAll'
+        },
+        'columns': [
+            {'data': 'name', 'width': '60%'},
+            {
+                'data': 'id',
+                'render': function(data) {
+                    return `<div class="text-center">
+                                <a href="/Upsert/${data}" class="btn btn-success text-white" style="cursor: pointer"><i class="fas fa-edit"></i></a>
+                                <a class="btn btn-danger text-white" style="cursor: pointer"><i class="fas fa-trash-alt"></i></a>
+                        </div>`;
+                },
+                'width': '40%'
+            },
+        ]
+    });
+}
