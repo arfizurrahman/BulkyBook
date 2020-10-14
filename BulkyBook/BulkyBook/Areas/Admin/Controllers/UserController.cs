7718b060f4a7163ed11953e9d955980969ab7d67
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-14 23:53:59 +0600

    Implemented get all user api in UserController

---
+++ BulkyBook/BulkyBook/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _db.ApplicationUsers.Include(a => a.Company).ToList();
+            var userRoles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            foreach (var user in users)
+            {
+                var roleId = userRoles.First(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.First(r => r.Id == roleId).Name;
+                if (user.Company == null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = "",
+                    };
+                }
+            }
+            return Json(new { data = users });
+        }
+
+        #endregion
+    }
+}
