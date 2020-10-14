4e2c76b51d86c4166d342a10464879c05ac5698c
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-14 09:18:55 +0600

    Added company js

--- BulkyBook/BulkyBook.Models/ApplicationUser.cs
+++ BulkyBook/BulkyBook.Models/ApplicationUser.cs
@@ -12,7 +12,10 @@ namespace BulkyBook.Models
         public string City { get; set; }
         public string State { get; set; }
         public string PostalCode { get; set; }
+        public int? CompanyId { get; set; }
 
+        [ForeignKey("CompanyId")]
+        public Company Company { get; set; }
         [NotMapped]
         public string Role { get; set; }
 
