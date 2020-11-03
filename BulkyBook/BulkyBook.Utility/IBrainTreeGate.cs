c935cf29eaf3ee73e9e5b805897c6b365349f72c
Mohammad Arfizur Rahman <arfizrahman0@gmail.com>
2020-11-03 21:25:18 +0600

    Add brainTree to DI

---
+++ BulkyBook/BulkyBook.Utility/IBrainTreeGate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Braintree;
+
+namespace BulkyBook.Utility
+{
+    public interface IBrainTreeGate
+    {
+        IBraintreeGateway CreateGateway();
+        IBraintreeGateway GetGateway();
+    }
+}
